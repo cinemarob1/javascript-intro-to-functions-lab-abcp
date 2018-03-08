7cfedaee5d08afd251ec896b32c4720cb5e834d3
Robert H Boscacci <cinemarob1@gmail.com>
2018-03-08 16:26:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,11 +14,8 @@ function logWhisper(string){
   console.log(string.toLowerCase());
 }
 
-var uppercase = "HELLO!"
-var lowercase = "hello!"
-
 function sayHiToGrandma(string){
-  if(lowercase = "hello!"){
-    return("I can't hear you!"")
+  if(lowercase = 'hello!'){
+    console.log('made it this far)
   }
 }
