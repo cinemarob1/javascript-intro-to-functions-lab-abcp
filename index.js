2c1549d26ca27518ca2459d667b98d80b72d36ea
Robert H Boscacci <cinemarob1@gmail.com>
2018-03-08 16:35:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function sayHiToGrandma(string){
   if(string = 'hello'){
     return("I can't hear you!");
   }
-  if(string = 'HELLO'){
+  else if(string = 'HELLO'){
     return("YES INDEED!");
   }
 }
